af57179854bb44dff9199977677b40bbb7e8b05f
MAAF72 <42692284+MAAF72@users.noreply.github.com>
2020-01-07 17:51:47 +0700

    Update simple.php

--- simple.php
+++ simple.php
@@ -1,7 +1,7 @@
 <?php
 $secret = 1282;
 $value = mt_rand(10000, 99999999);
-while ($value % $secrer != 0) {
+while ($value % $secret != 0) {
   $value = mt_rand(10000, 99999999);
 }
 echo "Random value is " . $value;
